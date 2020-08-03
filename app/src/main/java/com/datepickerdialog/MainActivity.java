6a6159437762e4334a488224336cc6d48c1b601d
Naftali (那富多利) <Naftali-K@users.noreply.github.com>
2020-08-03 21:26:58 +0300

    Initial commit

---
+++ app/src/main/java/com/datepickerdialog/MainActivity.java
@@ -0,0 +1,55 @@
+package com.datepickerdialog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private  static  final  String TAG = "MainActivity";
+
+    private TextView mDisplayDate;
+    private DatePickerDialog.OnDateSetListener onDateSetListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDisplayDate = findViewById(R.id.tvDate);
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        MainActivity.this,
+                        android.R.style.Theme_Holo_Dialog_NoActionBar_MinWidth,
+                        onDateSetListener,
+                        year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month++;
+                String fullDate = dayOfMonth + "/" + month + "/" + year;
+                mDisplayDate.setText(fullDate);
+            }
+        };
+    }
+}
